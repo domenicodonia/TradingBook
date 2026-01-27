d39d969c4d91168019cd9ea964d519b5c350a9d5
Domenico.Donia <domenicodonia@gmail.com>
2026-01-27 16:57:31 +0100

    Bootstrap refactoring

---
+++ TradingBook.Infrastructure/Bootstrap/InfrastructureBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using TradingBook.Infrastructure.Persistence;
+
+
+namespace TradingBook.Infrastructure.Bootstrap;
+
+public static class InfrastructureBootstrapper
+{
+    public static IDbContextFactory<TradingBookDbContext> CreateDbContextFactory(string dbPath)
+    {
+        var options = new DbContextOptionsBuilder<TradingBookDbContext>()
+            .UseSqlite($"Data Source={dbPath}")
+            .Options;
+
+        return new PooledDbContextFactory<TradingBookDbContext>(options);
+    }
+
+    public static void Migrate(IDbContextFactory<TradingBookDbContext> dbContextFactory)
+    {
+        using var db = dbContextFactory.CreateDbContext();
+        db.Database.Migrate();
+    }
+}
